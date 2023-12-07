9f39ef80ce3199b4ce439cd73b52206e2f4d258f
peninah98 <peninangizwenayo@gmail.com>
2023-12-07 17:50:34 +0200

    feat: haha

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dtos';
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    return body;
+    const userCredentials = this.userService.create(body.email, body.password);
+    return userCredentials;
   }
 }
